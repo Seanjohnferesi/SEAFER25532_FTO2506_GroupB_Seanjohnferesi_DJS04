389848aba732435ecf042b93ed4105fe5271048d
Seanjohnferesi <seanjohnferesi27@gmail.com>
2025-11-14 18:04:55 +0200

    feat: add expanding search bar on focus

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import "../styles/Header.css"
+import searchIcon from "../assets/search.png"
+import appIcon from "../assets/app-icon.png"
+import "../styles/styles.css"
+import man from "../assets/man.png"
+
+export default function Header() {
+    return (
+        <header className="app-header">
+            <div className="icon-name-wrapper">
+                <img src={appIcon} alt="app icon" />
+                <h1 className="header-title">Forger Talks</h1>
+            </div>
+
+            <div className="profile-wrapper">
+                <input type="text" name="search" className="search-bar" placeholder="Search"/>
+                <img src={searchIcon} className="search-icon" alt="search icon" />
+                
+                <img src={man} className ="profile" alt="a man" />
+            </div>
+        </header>
+    )
+}
